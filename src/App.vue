3499d9c7edc8e8aacda256b8a2527b7bf53938fb
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-21 18:09:37 +0900

    [mod] decided scroll-target for header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <router-view />
-      </v-container>
+      <Header />
+      <v-card class="overflow-hidden">
+        <v-sheet
+          id="scrolling-techniques-3"
+          class="overflow-y-auto"
+          max-height="600"
+        >
+          <v-container style="height: 2000px">
+            <router-view />
+          </v-container>
+        </v-sheet>
+      </v-card>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Header from '@/components/Header';
 export default {
   name: 'App',
 
-  components: {},
+  components: {
+    Header,
+  },
 
   data: () => ({
     //
