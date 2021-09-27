9f2757692c524b55282b2c2810d0070438ea9c2b
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-27 17:29:45 +0900

    [mod] created toggleSideNav for Hamburger by using Vuex

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,14 +1,12 @@
 <template>
   <v-sheet height="400" class="overflow-hidden">
-    <v-container class="fill-height">
-      <v-row align="center" justify="center">
-        <v-btn color="pink" dark @click.stop="drawer = !drawer">
-          Toggle
-        </v-btn>
-      </v-row>
-    </v-container>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary right>
+    <v-navigation-drawer
+      v-model="$store.state.drawer"
+      absolute
+      temporary
+      right
+      style="position: fixed; right:0; top:0; z-index:200;"
+    >
       <v-list dense>
         <v-list-item v-for="item in items" :key="item.title" link>
           <v-list-item-icon>
