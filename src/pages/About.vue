1e78bd95166cd7882df241465dbef4f0d347c806
Chukoto <70996691+Chukoto@users.noreply.github.com>
2022-04-13 01:48:28 +0900

    [fix] fixed my introduction

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,9 +8,7 @@
       <v-row>
         <v-col cols="12" sm="1" class="text-center">
           <p>
-            <img
-              src="https://img.icons8.com/color/100/000000/document--v1.png"
-            />
+            <img src="https://img.icons8.com/color/100/000000/document--v1.png" />
           </p>
         </v-col>
         <v-col class="hidden-xs-only">
@@ -25,10 +23,8 @@
       <v-row>
         <v-col class="hidden-sm-and-up">
           <p class="text-center">
-            ここはエンジニアHarukiのポートフォリオサイトです。
-            これまでに手がけた制作物、身につけたスキルをまとめています。
-            皆さんに、より私のことを知ってほしいと思って作りました。
-            どうぞ、ごゆっくりとサイト内を散策してください。
+            ここはエンジニアHarukiのポートフォリオサイトです。 これまでに手がけた制作物、身につけたスキルをまとめています。
+            皆さんに、より私のことを知ってほしいと思って作りました。 どうぞ、ごゆっくりとサイト内を散策してください。
           </p>
         </v-col>
       </v-row>
@@ -45,9 +41,7 @@
         <v-row>
           <v-col cols="12" sm="1" class="text-center">
             <p>
-              <img
-                src="https://img.icons8.com/clouds/100/000000/kawaii-bread-1.png"
-              />
+              <img src="https://img.icons8.com/clouds/100/000000/kawaii-bread-1.png" />
             </p>
           </v-col>
           <v-col class="hidden-xs-only" cols="11">
@@ -55,15 +49,17 @@
               神奈川県出身のエンジニア。大学で外国語学部・英語英文学科を専攻。<br />
               卒業後は電子部品を扱う専門商社に入社。<br />
               その後、IT技術に惹かれ、IT業界へ転身。<br />
-              バックエンドエンジニアとして入社したものの、フロントエンドに興味を持っている。
+              バックエンド、PM、PMO、テスターを経験した後、フロントエンドを担当。
+              提案型の営業や、ゲーム開発でユーザーの年代層やプレイしていただく時間帯を考慮した開発経験から、
+              今後もユーザーに寄り添った形で開発ができるフロントエンドエンジニアを目指す。
             </p>
           </v-col>
           <v-col class="hidden-sm-and-up" cols="12">
             <p>
-              神奈川県出身のエンジニア。大学で外国語学部・英語英文学科を専攻。
-              卒業後は電子部品を扱う専門商社に入社。
-              その後、IT技術に惹かれ、IT業界へ転身。
-              バックエンドエンジニアとして入社したものの、フロントエンドに興味を持っている。
+              神奈川県出身のエンジニア。大学で外国語学部・英語英文学科を専攻。 卒業後は電子部品を扱う専門商社に入社。
+              その後、IT技術に惹かれ、IT業界へ転身。 バックエンド、PM、PMO、テスターを経験した後、フロントエンドを担当。
+              提案型の営業や、ゲーム開発でユーザーの年代層やプレイしていただく時間帯を考慮した開発経験から、
+              今後もユーザーに寄り添った形で開発ができるフロントエンドエンジニアを目指す。
             </p>
           </v-col>
         </v-row>
@@ -71,15 +67,11 @@
     </div>
 
     <div>
-      <a
-        href="https://icons8.com/icon/ofy7JDxeW34D/kawaii-bread"
-        class="text-caption cyan--text text--lighten-5"
+      <a href="https://icons8.com/icon/ofy7JDxeW34D/kawaii-bread" class="text-caption cyan--text text--lighten-5"
         >Kawaii Soda icon by Icons8
       </a>
       <br />
-      <a
-        href="https://icons8.com/icon/12053/document"
-        class="text-caption cyan--text text--lighten-5"
+      <a href="https://icons8.com/icon/12053/document" class="text-caption cyan--text text--lighten-5"
         >Document icon by Icons8
       </a>
     </div>
